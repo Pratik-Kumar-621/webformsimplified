b5c5aa21de4c2b3a58d64c707b31db59f0101a4a
Pratik Kumar <70286186+Pratik-Kumar-621@users.noreply.github.com>
2026-03-17 04:48:03 +0530

    Dotted background addition

--- components/landing/features/Features.tsx
+++ components/landing/features/Features.tsx
@@ -40,7 +40,11 @@ const features = [
   },
 ];
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="relative z-20 min-h-40 shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] dark:shadow-[-10px_-10px_30px_4px_rgba(255,255,255,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]">
+      Features
+    </div>
+  );
 };
 
 export default Features;
